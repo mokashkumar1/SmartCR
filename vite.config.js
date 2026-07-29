88384ac78f7c8d036a0567674b0d8203fc8bcad4
Mokash Kumar <mokshkumar38@gmail.com>
2026-07-29 17:29:17 +0500

    Activate PWA updates immediately

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+        cleanupOutdatedCaches: true,
+      },
       includeAssets: ['pwa-icon.svg'],
       manifest: {
         name: 'Smart CR',
